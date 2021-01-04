39c4416896b7c7841aab0d977335dd8c77e363ba
Andrew Perera <pererasys@gmail.com>
2021-01-04 00:18:26 -0500

    add CI

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,11 @@ module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
   modulePathIgnorePatterns: ["__utils"],
+  reporters: [
+    "default",
+    [
+      "jest-junit",
+      { outputDirectory: "test-results/junit", outputName: "results.xml" },
+    ],
+  ],
 };
